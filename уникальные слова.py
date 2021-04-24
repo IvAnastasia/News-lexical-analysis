659420c10d2ab68e325ef87263f4d916e9dd01cb
IvAnastasia <71773596+IvAnastasia@users.noreply.github.com>
2021-04-24 13:08:51 +0300

    Update уникальные слова.py

--- уникальные слова.py
+++ уникальные слова.py
@@ -25,3 +25,32 @@ def unique_words_percent(text: str):
      # процент уникальных слов       
     return len(unique_words) / len(all_words) * 100
 
+#сравним по всем новостям сразу
+with open('rusnews.txt', encoding='utf-8') as file:
+    text = file.read()
+    print('Процент уникальных слов в гос. новостях: ', unique_words_percent(text))
+with open('indepnews.txt', encoding='utf-8') as file:
+    text = file.read()
+    print('Процент уникальных слов в независимых новостях: ', unique_words_percent(text))
+
+#сравним по каждой новости отдельно
+rus = 0
+indep = 0
+for n in range(1, 2):
+    print('Текст №' + str(n))
+    with open('rusnew' + str(n) + '.txt', encoding='utf-8') as file:
+        text = file.read()
+        rus_uniq = unique_words_percent(text)
+        print('Процент уникальных слов в этой гос. новости: ', rus_uniq)
+    with open('indepnew' + str(n) +'.txt', encoding='utf-8') as file:
+        text = file.read()
+        indep_uniq = unique_words_percent(text)
+        print('Процент уникальных слов в этой независимой новости: ', indep_uniq)
+    if rus_uniq > indep_uniq:
+        rus += 1
+    else:
+        indep += 1
+if rus > indep:
+    print('В целом гос. новостей, в которых уникальных слов больше, больше: ', rus, 'vs.', indep)
+elif rus < indep:
+    print('В целом независимых новостей, в которых уникальных слов больше, больше: ', indep, 'vs.', rus)
