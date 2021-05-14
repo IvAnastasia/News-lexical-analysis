7953a8bd81226378e5ba5c782c52aee87f8880f4
eaverr <71773445+eaverr@users.noreply.github.com>
2021-05-14 14:18:47 +0300

    Update среднее количество предложений.py

--- среднее количество предложений.py
+++ среднее количество предложений.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import re
 
 # среднее количество предложений в текстах
@@ -11,10 +8,10 @@ def mean_sentence_number(text):
 rus_sum = 0
 indep_sum = 0
 for n in range(1, 5):
-    with open('rusnew' + str(n) + '.txt') as file:
+    with open('rusnew' + str(n) + '.txt', encoding = 'utf-8') as file:
         text = file.read()
         rus_sum += mean_sentence_number(text)
-    with open('indepnew' + str(n) + '.txt') as file:
+    with open('indepnew' + str(n) + '.txt', encoding = 'utf-8') as file:
         text = file.read()
         indep_sum += mean_sentence_number(text)
 
@@ -22,6 +19,6 @@ rus = rus_sum/4
 indep = indep_sum/4
 
 if rus > indep:
-    print('В гос. новостях в среднем предложений больше')
+    print('В гос. новостях в среднем предложений больше.')
 elif indep > rus:
-    print('В независимых новостях в среднем предложений больше')
+    print('В независимых новостях в среднем предложений больше.')
