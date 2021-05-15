b6340d19ebd27a41d3a8afa5c0c48c5476c4b52e
IvAnastasia <71773596+IvAnastasia@users.noreply.github.com>
2021-05-15 08:48:47 +0300

    Update POS.py

--- POS.py
+++ POS.py
@@ -1,4 +1,4 @@
-pip install pymorphy2
+import re
 import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
 
@@ -8,7 +8,8 @@ def get_POS(word):
 
 def POS_freq_dict(text_name):
     freq_dict = {}
-    with open(text_name + '.txt, encoding = 'utf-8'') as file:
+    words = []
+    with open(text_name + '.txt', encoding = 'utf-8') as file:
         text = file.read()
         dirty_words = re.findall("\w+", text)
         for dirty_word in dirty_words:
@@ -25,7 +26,21 @@ def POS_freq_dict(text_name):
 def words_number(di):
     return sum(di.values())
 
+#сравним по частотности разные частей речи по каждой из гос. новостей и соответствующей ей независимой новости
+for n in range(1, 17):
+    print('Текст №' + str(n))
+    rus_POS = POS_freq_dict('rusnew' + str(n))
+    indep_POS = POS_freq_dict('indepnew' + str(n))
+    for key in rus_POS.keys():
+        if key in indep_POS.keys():
+            print(key, ' (в абсолютных числах):', 'в этой гос.новости ', rus_POS[key], ',', 'в независимой новости ',
+                  indep_POS[key])
+            print(key, ' (по доли употребления, т.е. в зависимости от объёмов текста):', 'в этой гос.новости ',
+                  rus_POS[key] / words_number(rus_POS), ',', 'в этой независимых новости ',
+                  indep_POS[key] / words_number(indep_POS))
+
 #сравним по частотности разные частей речи по всем гос. новостям и по всем независимым новостям
+print('Всего')
 rus_POS = POS_freq_dict('rusnews')
 indep_POS = POS_freq_dict('indepnews')
 for key in rus_POS.keys():
